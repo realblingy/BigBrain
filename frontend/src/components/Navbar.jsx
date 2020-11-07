2c136f89a38cbd2246bb4a3a7c2e78cabe8e9bec
James Dang <jamesphidang@gmail.com>
2020-11-07 21:30:25 +1100

    Started on dashboard

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  AppBar, Toolbar, Typography, Button, IconButton, makeStyles,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#1C204B',
+    color: 'white',
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+function Navbar() {
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+        <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Dashboard
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import { CircularProgress } from '@material-ui/core';
 import Logout from '../components/LogoutButton';
+import Navbar from '../components/Navbar';
+import port from '../api';
 
 function Dashboard(props) {
   const { setToken, token } = props;
+  const [quizzes, setQuizzes] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
   if (token === '') {
     return <Redirect to="/" />;
   }
 
+  const getQuizzes = async () => {
+    const response = await fetch(`${port}/admin/quiz`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.status === 200) {
+      const responseData = await response.json();
+      return responseData.quizzes;
+    }
+    throw new Error('Could not load quizzes.');
+  };
+
+  React.useEffect(async () => {
+    try {
+      setLoading(true);
+      const data = await getQuizzes();
+      setQuizzes(JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div>
-      DashBoard
+      <Navbar />
+      {loading ? <CircularProgress color="primary" /> : <h1>{quizzes}</h1>}
       <Logout setToken={setToken} token={token} />
     </div>
   );
