6cbf8040af88662541dcb4d7735d2b52da84f41c
James Dang <jamesphidang@gmail.com>
2020-11-20 20:00:26 +1100

    Removed stories and temporarily commented out Game.jsx

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { useParams } from 'react-router-dom';
-import { getPlayerQuestion, getPlayerStatus } from '../api';
-import GamePanel from '../components/GamePanel';
+// import React from 'react';
+// import Typography from '@material-ui/core/Typography';
+// import { useParams } from 'react-router-dom';
+// import { getPlayerQuestion, getPlayerStatus } from '../api';
+// import GamePanel from '../components/GamePanel';
 
-function Game() {
-  const { quizID, playerID } = useParams();
-  const [errMsg, setErrMsg] = React.useState('');
-  const [currQuestion, setCurrQuestion] = React.useState(null);
+// function Game() {
+//   const { quizID, playerID } = useParams();
+//   const [errMsg, setErrMsg] = React.useState('');
+//   const [currQuestion, setCurrQuestion] = React.useState(null);
 
-  React.useEffect(() => {
-    const constantFetch = setInterval(() => {
-      getPlayerStatus(playerID).then((r) => {
-        if (r.started === true) {
-          getPlayerQuestion(playerID).then((res) => {
-            setErrMsg('');
-            setCurrQuestion(res.question);
-          });
-          clearInterval(constantFetch);
-        } else {
-          setErrMsg('Waiting for host');
-        }
-      });
-    }, 1000);
-  }, [playerID]);
+//   React.useEffect(() => {
+//     const constantFetch = setInterval(() => {
+//       getPlayerStatus(playerID).then((r) => {
+//         if (r.started === true) {
+//           getPlayerQuestion(playerID).then((res) => {
+//             setErrMsg('');
+//             setCurrQuestion(res.question);
+//           });
+//           clearInterval(constantFetch);
+//         } else {
+//           setErrMsg('Waiting for host');
+//         }
+//       });
+//     }, 1000);
+//   }, [playerID]);
 
-  return (
-    <div>
-      <Typography>{errMsg}</Typography>
-      {currQuestion !== null
-      && (
-        <GamePanel
-          answers={currQuestion.answers}
-          correctAnswers={currQuestion.correctAnswers}
-          isoTimeLastQuestionStarted={currQuestion.isoTimeLastQuestionStarted}
-          points={currQuestion.points}
-          question={currQuestion.question}
-          timer={currQuestion.timer}
-          playerID={playerID}
-          quizID={quizID}
-          setCurrQuestion={setCurrQuestion}
-        />
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Typography>{errMsg}</Typography>
+//       {currQuestion !== null
+//       && (
+//         <GamePanel
+//           answers={currQuestion.answers}
+//           correctAnswers={currQuestion.correctAnswers}
+//           isoTimeLastQuestionStarted={currQuestion.isoTimeLastQuestionStarted}
+//           points={currQuestion.points}
+//           question={currQuestion.question}
+//           timer={currQuestion.timer}
+//           playerID={playerID}
+//           quizID={quizID}
+//           setCurrQuestion={setCurrQuestion}
+//         />
+//       )}
+//     </div>
+//   );
+// }
 
-export default Game;
+// export default Game;
